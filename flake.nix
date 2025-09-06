5225eb2ffc437b03d3c514c1db201eb1840b6de5
torstenboettjer <torsten.boettjer@gmail.com>
2025-09-06 14:07:47 +0200

    Initial commit of project files

---
+++ flake.nix
@@ -0,0 +1,28 @@
+# flake.nix "flake" "imports"
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    # Development
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, devshell }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
+    imports = [];
+
+    flake = {
+      # your existing definitions before using flake-parts...
+    };
+  };
+}
